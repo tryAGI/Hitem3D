70b46d9f3c996b292f40102823aef9a44ecd76b2
Konstantin S. <havendv@gmail.com>
2026-05-05 20:31:01 +0400

    feat: Updated OpenAPI spec (#4)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Hitem3D/Generated/Hitem3D.ITasksClient.QueryTask.g.cs
+++ src/libs/Hitem3D/Generated/Hitem3D.ITasksClient.QueryTask.g.cs
@@ -15,5 +15,16 @@ namespace Hitem3D
             string taskId,
             global::Hitem3D.AutoSDKRequestOptions? requestOptions = default,
             global::System.Threading.CancellationToken cancellationToken = default);
+        /// <summary>
+        /// Query a task
+        /// </summary>
+        /// <param name="taskId"></param>
+        /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
+        /// <param name="cancellationToken">The token to cancel the operation with</param>
+        /// <exception cref="global::Hitem3D.ApiException"></exception>
+        global::System.Threading.Tasks.Task<global::Hitem3D.AutoSDKHttpResponse<global::Hitem3D.TaskEnvelope>> QueryTaskAsResponseAsync(
+            string taskId,
+            global::Hitem3D.AutoSDKRequestOptions? requestOptions = default,
+            global::System.Threading.CancellationToken cancellationToken = default);
     }
 }
